d4c6028a08c8b24e9748d47a3226c3a633a20cd9
Lokeshrawani <lokeshrawani122@gmail.com>
2025-06-14 18:46:51 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // Print message to the console
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!NEED some changes");
     }
 }
 	
